46908f0d6df859a1d440904be2b108166a6dede5
Oleksii <jackinsonalex@gmail.com>
2023-10-23 16:46:25 +0300

    added token

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import asyncio
+from hashlib import md5
+
 from mongo_db import db
 from fastapi.responses import HTMLResponse, RedirectResponse
 from typing import Annotated, Optional
@@ -14,7 +17,7 @@ fake_users_db = db.user
 
 
 def fake_hash_password(password: str):
-    return 'fakehashed' + password
+    return md5(password.encode('utf-8')).hexdigest()
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -31,21 +34,21 @@ class UserInDB(User):
     hashed_password: str
 
 
-def get_user(database, username: str):
-    if username in database:
-        user_dict = database[username]
-        return UserInDB(**user_dict)
+async def get_user(database, user_name: str):
+    user_dict = await database.find_one({f"{user_name}.username": user_name})
+    if user_dict:
+        user_data = user_dict[user_name]
+        return UserInDB(**user_data)
+    return None
 
 
-def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
-    user = get_user(fake_users_db, token)
+async def fake_decode_token(token):
+    user = await get_user(fake_users_db, token)
     return user
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    user = fake_decode_token(token)
+    user = await fake_decode_token(token)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -55,7 +58,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     return user
 
 
-async def get_current_active_user(
+def get_current_active_user(
     current_user: Annotated[User, Depends(get_current_user)]
 ):
     if current_user.disabled:
@@ -67,7 +70,6 @@ async def get_current_active_user(
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     name = form_data.username
     user_dict = await fake_users_db.find_one({f"{name}.username": form_data.username})
-    print(user_dict)
     if not user_dict:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     user_data = user_dict[name]
@@ -108,9 +110,10 @@ async def read_users_me(
 ):
     return current_user
 
+
 @app.post("/")
-async def root(long_url: Annotated[str, Form()], short_url: Optional[str] = Form(None)):
-    url = await create_short_url(long_url, short_url)
+async def root(current_user: Annotated[User, Depends(get_current_active_user)], long_url: Annotated[str, Form()], short_url: Optional[str] = Form(None)):
+    url = await create_short_url(long_url, short_url, user_id=current_user.username)
     return url
 
 
@@ -124,10 +127,10 @@ async def to_long(short_url: str):
 
 
 @app.post("/{short_url}")
-async def update_short_url(short_url: str, new_long_url: Annotated[str, Form()]):
+async def update_short_url(current_user: Annotated[User, Depends(get_current_active_user)], short_url: str, new_long_url: Annotated[str, Form()]):
     original_link = await db.links.find_one({'short_url': short_url})
     new_long_url = {'long_url': new_long_url}
-    answer = await db.links.update_one({'_id': original_link['_id']}, {'$set': new_long_url})
+    answer = await db.links.update_one({'_id': original_link['_id'], 'user_id': current_user.username}, {'$set': new_long_url})
     if answer.modified_count == 1:
         return {'short_url': short_url, 'long_url': new_long_url}
     return {'error': 'Nothing Modified'}
